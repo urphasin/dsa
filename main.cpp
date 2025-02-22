ca97618a38ed14138f46be448618424e7c19904b
Otito Amuga <otitoamuga@gmail.com>
2025-02-22 17:23:09 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <cmath>
+
+template<typename T>
+void print(T collection, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << collection[i] << std::endl;
+    }
+}
+
+int main() {
+    
+    std::cout << pow(3, 5) << std::endl;
+}
